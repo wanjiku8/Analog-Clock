92d1f4e2440e0a119e7378be7c3c82a147038f7d
wanjiku8 <wanjikuf141@gmail.com>
2023-11-29 12:51:09 +0300

    commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const numberHours =document.querySelector('.number-hours');
+
+const barSeconds =document.querySelector('.bar-seconds');
+
+const numberElement =[];
+const barElement =[];
+
+// create number hours
+for (let i = 1; i <= 12; i++) {
+    numberElement.push(
+        `<span style="--index:${i};"><p>${i}</p></span>`
+    );
+}
+numberHours.insertAdjacentHTML("afterbegin", numberElement.join(""));
+console.log(numberElement);
+// creating bar seconds
+for (let i = 1; i <= 60; i++) {
+    barElement.push(
+        `<span style="--index:${i};"><p></p></span>`
+    );
+   
+}
+barSeconds.insertAdjacentHTML("afterbegin", barElement.join(""));
+console.log(barElement);
+
+const handHours = document.querySelector('.hand.hours');
+const handMinutes = document.querySelector('.hand.minutes');
+const handSeconds= document.querySelector('.hand.seconds');
+
+function getCurrentTime() {
+    let date = new Date();
+    let currentHours = date.getHours();
+    let currentMinutes = date.getMinutes();
+    let currentSeconds = date.getSeconds();
+
+    handSeconds.style.transform = `rotate(${currentSeconds * 6}deg)`;
+
+    handMinutes.style.transform = `rotate(${currentMinutes * 6}deg)`;
+    handHours.style.transform = `rotate(${currentHours * 30+ currentMinutes /2}deg)`;
+
+
+}
+// call currentTime function on page load
+getCurrentTime();
+//  call getCurrentTime to set clock hand every second
+setInterval(getCurrentTime, 1000);// 1000ms = 1s
+
+
+
+
+
